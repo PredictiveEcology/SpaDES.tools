beb2cfbe393e1f93ac9fe86ccba83e2a69e2bac4
Alex Chubaty <alex.chubaty@gmail.com>
2018-07-05 15:37:00 -0600

    cleanup test-examples.R

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,6 +1,7 @@
 test_that("all exported functions have examples", {
-  tmpExFile <- "C:/Eliot/tmp/examplesSpaDES.tools.txt"
-  if (grepl("VIC-", Sys.info()["nodename"]))  { # for debugging only
+  if (grepl("VIC-", Sys.info()["nodename"]))  {
+    # for debugging only
+    tmpExFile <- "C:/Eliot/tmp/examplesSpaDES.tools.txt"
     cat("#START##############\n", file = tmpExFile, append = FALSE)
   }
 
@@ -11,20 +12,22 @@ test_that("all exported functions have examples", {
   setwd(tmpdir)
   on.exit({
     unlink(tmpdir, recursive = TRUE)
-    setwd(owd)}
-    , add = TRUE)
-  if (grepl("VIC-", Sys.info()["nodename"])) { # for debugging only
-    cat(paste("All files exist: ", isTRUE(all(file.exists(exFiles))), "\n"), file = tmpExFile, append = TRUE)
+    setwd(owd)
+  } , add = TRUE)
 
+  if (grepl("VIC-", Sys.info()["nodename"])) {
+    # for debugging only
+    cat(paste("All files exist: ", isTRUE(all(file.exists(exFiles))), "\n"),
+        file = tmpExFile, append = TRUE)
   }
 
   for (file in exFiles) {
-    if (grepl("VIC-", Sys.info()["nodename"])) { # for debugging only
+    if (grepl("VIC-", Sys.info()["nodename"])) {
+      # for debugging only
       cat(paste(file, " -- ", "\n"), file = tmpExFile, append = TRUE)
     }
     # for debugging only
     print(file)
     test_example(file)
   }
-
 })
