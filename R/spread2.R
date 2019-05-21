70375e9be9644d2ef1d94554aa6878ea81e0f0d8
Eliot McIntire <eliotmcintire@gmail.com>
2019-05-21 16:44:53 -0700

    spread2 -- add arg for maxRetriesPerID

--- R/spread2.R
+++ R/spread2.R
@@ -84,9 +84,9 @@ if (getRversion() >= "3.1.0") {
 #'                       on during a spreading event. This will override an event that
 #'                       stops probabilistically via \code{spreadProb}, but forcing
 #'                       its last set of active cells to try again to find neighbours.
-#'                       It will try 10 times per event, before giving up.
-#'                       During those 10 times, it will try twice to "jump" up to
-#'                       4 cells outwards from each of the active cells.\cr
+#'                       It will try \code{maxRetriesPerID} times per event, before giving up.
+#'                       During those \code{maxRetriesPerID} times, it will try to "jump" up to
+#'                       4 cells outwards from each of the active cells, every 5 retries.\cr
 #'   \code{iterations} \tab This is a hard cap on the number of internal iterations to
 #'                          complete before returning the current state of the system
 #'                          as a \code{data.table}.\cr
@@ -187,6 +187,10 @@ if (getRversion() >= "3.1.0") {
 #'                   spread iteration will spread to \code{1, 2, ..., length(neighProbs)}
 #'                   neighbours, respectively. If this is used (i.e., something other than
 #'                   NA), \code{circle} and \code{returnDistances} will not work currently.
+#' @param maxRetriesPerID Only active if \code{exactSize} is used. This is the number of attempts
+#'                        that will be made per event ID, before abandoning, therefore completing
+#'                        the spread2 for that event with a size that is smaller than
+#'                        \code{exactSize}. Default 10 times.
 #'
 #' @param asymmetry     A numeric or \code{RasterLayer} indicating the ratio of the
 #'                      asymmetry to be used. i.e., 1 is no asymmetry; 2 means that the
@@ -345,7 +349,7 @@ spread2 <- function(landscape, start = ncell(landscape) / 2 - ncol(landscape) / 
                     spreadProb = 0.23, persistProb = NA_real_, asRaster = TRUE,
                     maxSize, exactSize, directions = 8L, iterations = 1e6L,
                     returnDistances = FALSE, returnDirections = FALSE,
-                    returnFrom = FALSE,
+                    returnFrom = FALSE, maxRetriesPerID = 10,
                     spreadProbRel = NA_real_, plot.it = FALSE, circle = FALSE,
                     asymmetry = NA_real_, asymmetryAngle = NA_real_,
                     allowOverlap = 0, neighProbs = NA_real_, skipChecks = FALSE) {
@@ -435,7 +439,7 @@ spread2 <- function(landscape, start = ncell(landscape) / 2 - ncol(landscape) / 
   usingAsymmetry <- !is.na(asymmetry)
 
   # This means that if an event can not spread any more, it will try 10 times, incl. 2 jumps
-  maxRetriesPerID <- 10
+  # maxRetriesPerID <- 10
 
   if (!is.numeric(start) & !is.data.table(start)) {
     if (is(start, "Raster")) {
