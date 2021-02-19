46690fa95056514b2a22d065b9c29d05f9c8bcb4
Alex Chubaty <achubaty@for-cast.ca>
2021-02-18 23:06:56 -0700

    fix Rcpp stuff

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -54,3 +54,16 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_SpaDES_tools_duplicatedInt", (DL_FUNC) &_SpaDES_tools_duplicatedInt, 1},
+    {"_SpaDES_tools_pointDistance2", (DL_FUNC) &_SpaDES_tools_pointDistance2, 2},
+    {"_SpaDES_tools_pointDistance3", (DL_FUNC) &_SpaDES_tools_pointDistance3, 5},
+    {"_SpaDES_tools_runifC", (DL_FUNC) &_SpaDES_tools_runifC, 1},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_SpaDES_tools(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
