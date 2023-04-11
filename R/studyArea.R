3131c5eb5234973204a182d962475e02903f694f
CeresBarros <cbarros@mail.ubc.ca>
2023-04-10 21:32:20 -0700

    terrorize randomStudyArea

--- R/studyArea.R
+++ R/studyArea.R
@@ -12,13 +12,18 @@
 #' @return `SpatalPolygonsDataFrame`
 #'
 #' @export
-#' @importFrom sp SpatialPolygonsDataFrame
-#' @importFrom sf st_crs st_sfc st_point
+#' @importFrom terra vect crs values
+#'
+#' @example
+#' a <- randomStudyArea(seed = 123)
+#' if (interactive) {
+#'   terra::plot(a)
+#' }
 randomStudyArea <- function(center = NULL, size = 1e4, seed = NULL) {
   if (is.null(center)) {
-    center <- st_sfc(st_point(c(-1349980, 6986895)))
-    st_crs(center) <- st_crs(paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0",
-                                "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+    center <- vect(cbind(-1349980, 6986895))
+    crs(center) <- paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0",
+                         "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
   }
 
   if (!exists(".Random.seed", envir = .GlobalEnv)) set.seed(NULL)
@@ -26,17 +31,16 @@ randomStudyArea <- function(center = NULL, size = 1e4, seed = NULL) {
 
   if (!is.null(seed))
     set.seed(seed)
-  studyArea <- SpaDES.tools::randomPolygon(x = center, area = size)
+  studyArea <- randomPolygon(x = center, area = size)
   if (!is.null(seed))
     set.seed(prevSeed)
 
   dfData <- if (is.null(rownames(studyArea))) {
-    polyID <- sapply(slot(studyArea, "polygons"), function(x) slot(x, "ID"))
-    data.frame("field" = as.character(seq_along(length(studyArea))), row.names = polyID)
+    data.frame("field" = as.character(seq_along(length(studyArea))))
   } else {
-    polyID <- sapply(slot(studyArea, "polygons"), function(x) slot(x, "ID"))
-    data.frame("field" = rownames(studyArea), row.names = polyID)
+    data.frame("field" = rownames(studyArea))
   }
 
-  SpatialPolygonsDataFrame(studyArea, data = dfData)
+  values(studyArea) <- dfData
+  studyArea
 }
