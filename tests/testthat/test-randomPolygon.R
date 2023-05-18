257a4e4ac8bdcaf46bab55f0d2f436e32939a104
Eliot McIntire <eliotmci@mail.ubc.ca>
2023-05-18 13:10:06 -0700

    randomPolygon -- test needs `terra::plot`

--- tests/testthat/test-randomPolygon.R
+++ tests/testthat/test-randomPolygon.R
@@ -1,5 +1,6 @@
 test_that("randomPolygon: does not work properly", {
-  library(terra)
+  # no need for `raster` testing as this is using randomPolygon
+  withr::local_package("terra")
 
   set.seed(1234) ## TODO: some seeds produce failing area test below!!
   latLong <- crs("+init=epsg:4326")
@@ -8,17 +9,21 @@ test_that("randomPolygon: does not work properly", {
   center <- cbind(-110, 59)
   poly1 <- randomPolygon(center, area = area)
   if (interactive()) {
-    plot(poly1)
+    terra::plot(poly1)
   }
 
   poly1InUTM <- project(poly1, utmCRS(poly1))
   ## check that polygon area approximately matches that given by hectares
   polyArea <- expanse(poly1InUTM)
-  expect_true(base::abs(base::abs(polyArea - area)) <  area / 4) ## TODO: why is this area/4?
+
+  ## scale the test to area. This means in current area of 1e4, polygon area should be more than 2500; reasonable
+  expect_true(base::abs(base::abs(polyArea - area)) <  (area / 4))  ## TODO: why is this area/4?
 
   ## check that polygon center is approximately centered on x
   centerSP <- vect(center, crs = latLong)
   centerSP_UTM <- project(centerSP, crs(poly1InUTM))
   polyCenter <- terra::centroids(poly1InUTM)
-  expect_true(terra::distance(centerSP_UTM, polyCenter) < 100) ## TODO: appropriate test?
+
+  ## scale the test to area. This means in current area of 1e4, that centroid is 100 m from center; reasonable
+  expect_true(terra::distance(centerSP_UTM, polyCenter) < (area/100))
 })
