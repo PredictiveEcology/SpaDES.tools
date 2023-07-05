170065b15d3e7618135de773732e33a2686514b7
Eliot McIntire <eliotmcintire@gmail.com>
2023-07-05 07:25:37 -0700

    Revert "Deal with CRAN: Examples with CPU time > 2.5 times elapsed time"
    This reverts commit 082c4951c7a50f084d7f15414d0778d74fcc2a6f.

--- inst/examples/example_cir.R
+++ inst/examples/example_cir.R
@@ -1,5 +1,4 @@
 library(terra)
-origThreads <- data.table::setDTthreads(1) # for CRAN automated testing
 
 set.seed(1462)
 
@@ -52,7 +51,7 @@ if (interactive()) {
 }
 
 # rings
-if (requireNamespace("CircStats", quietly = TRUE)) {
+if (require("CircStats")) {
   loci <- cellFromXY(hab, crds(coords))
   cirs2 <- rings(hab, loci, maxRadius = radius, minRadius = radius - 1, returnIndices = TRUE)
 
@@ -84,5 +83,3 @@ circ <- cir(ras, coords, angles = seq(0, 2 * pi, length.out = 21),
             maxRadius = 200, minRadius = 0, returnIndices = FALSE,
             allowOverlap = TRUE, returnAngles = TRUE)
 
-# clean up
-data.table::setDTthreads(origThreads)
