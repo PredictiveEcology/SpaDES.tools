85911e86d0728b44e36f7665eda0ec77714d4efb
Alex Chubaty <achubaty@for-cast.ca>
2023-07-14 19:32:45 -0600

    partially revert a48fd73a443f5e9dca26a4d489bc96fd00cd1e1b
    setting `OMP_NUM_THREADS` from R gets ignored; needs to be set before R startup

--- R/zzz.R
+++ R/zzz.R
@@ -3,23 +3,6 @@
 ## -------------------------------------------------------------------------- ##
 
 .onLoad <- function(libname, pkgname) {
-
-  elapsedRatio <- Sys.getenv("_R_CHECK_EXAMPLE_TIMING_CPU_TO_ELAPSED_THRESHOLD_")
-  if (nzchar(elapsedRatio)) {
-    # CRAN allows use of up to 2 CPUs when OMP_NUM_THREADS is not
-    # set, but for some reason some examples finish too fast as if
-    # we're using more than 1 CPUs:
-    #
-    # "Examples with CPU time > 2.5 times elapsed time"
-    #
-    # To work around this issue, never use more than 1 CPU when
-    # OMP_NUM_THREADS is not set && _R_CHECK_EXAMPLE_TIMING_CPU_TO_ELAPSED_THRESHOLD_ is set.
-    options("spades.OMP_NUM_THREADS" = Sys.getenv("OMP_NUM_THREADS"))
-    options("spades.OMP_THREAD_LIMIT" = Sys.getenv("OMP_THREAD_LIMIT"))
-    Sys.setenv("OMP_NUM_THREADS"=1L)
-    Sys.setenv("OMP_THREAD_LIMIT"=1L)
-  }
-
   ## set options using the approach used by devtools
   opts <- options()
   opts.spades <- list( # nolint
@@ -37,12 +20,5 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive()) {
     packageStartupMessage("Using SpaDES.tools version ", packageVersion("SpaDES.tools"), ".")
-  }
-}
-
-.onDetach <- function(libname, pkgname) {
-  if (!is.null(getOption("spades.OMP_THREAD_LIMIT"))) {
-    Sys.setenv("OMP_THREAD_LIMIT" = getOption("spades.OMP_THREAD_LIMIT"))
-    Sys.setenv("OMP_NUM_THREADS" = getOption("spades.OMP_NUM_THREADS"))
   }
 }
