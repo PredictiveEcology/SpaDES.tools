PredictiveEcology/SpaDES.tools

commit 7b53fc64d214b76c9d5eb2e0c85336df48d124b1

misc cleanup
- de-lint;
- spelling;
- formatting;

R/neighbourhood.R
@@ -123,7 +123,7 @@ adj <- function(x = NULL, cells, directions = 8, sort = FALSE, pairs = TRUE,
   J <- NULL # nolint
   cells <- as.integer(cells)
 
-  if (is.null(numCol) | is.null(numCell)) {
+  if (is.null(numCol) || is.null(numCell)) {
     if (is.null(x)) stop("must provide either numCol & numCell or a x")
     numCol <- as.integer(ncol(x))
     numCell <- as.integer(ncell(x))
@@ -238,22 +238,14 @@ adj <- function(x = NULL, cells, directions = 8, sort = FALSE, pairs = TRUE,
     keepCols <- if (is.null(id)) "to" else c("to", "id")
     if (!torus) {
       if (pairs) {
-        # orig <- adj[
-        #   !((((adj[, "to"] - 1) %% numCell + 1) != adj[, "to"]) | # top or bottom of raster
-        #       ((adj[, "from"] %% numCol + adj[, "to"] %% numCol) == 1)) # | #right & left edge cells, with neighbours wrapped
-        #   , , drop = FALSE]
-        # return(orig)
-        #possNew <-
-          return(adj[
+        return(adj[
           !((adj[, "to"] <= 0 | adj[, "to"] > numCell)  | # top or bottom of raster
-              ((adj[, "from"] %% numCol + adj[, "to"] %% numCol) == 1)) # | #right & left edge cells, with neighbours wrapped
+              ((adj[, "from"] %% numCol + adj[, "to"] %% numCol) == 1)) # right & left edge cells, with neighbours wrapped
           , , drop = FALSE])
-        # if (!identical(orig, possNew)) stop("the new adj algorithm is not the same as the old")
-        #return(possNew)
       } else {
         adj <- adj[
           !((((adj[, "to"] - 1) %% numCell + 1) != adj[, "to"]) | # top or bottom of raster
-              ((adj[, "from"] %% numCol + adj[, "to"] %% numCol) == 1)) # | #right & left edge cells, with neighbours wrapped
+              ((adj[, "from"] %% numCol + adj[, "to"] %% numCol) == 1)) # right & left edge cells, with neighbours wrapped
           , keepCols, drop = FALSE]
         if (match.adjacent) {
           adj <- unique(adj[, "to"])
@@ -622,7 +614,7 @@ cir <- function(landscape, coords, loci,
 
   indices <- as.integer(cellFromXY(landscape, cbind(x, y)))
 
-  if (moreThanOne & allowOverlap & !closest) {
+  if (moreThanOne && allowOverlap && !closest) {
     matDT <- data.table(id, indices, rads, angles, x = x, y = y)
     setkeyv(matDT, c("id", "indices"))
     if (!equalRadii) {
@@ -636,7 +628,7 @@ cir <- function(landscape, coords, loci,
     matDT <- as.matrix(matDT)
   } else {
     matDT <- cbind(id, rads, angles, x, y, indices)
-    if (!closest & !allowDuplicates) {
+    if (!closest && !allowDuplicates) {
       notDups <- !duplicatedInt(indices)
       matDT <- matDT[notDups, , drop = FALSE]
     }
@@ -645,7 +637,7 @@ cir <- function(landscape, coords, loci,
   rm(id, indices, rads, x, y)
 
   # only need to calculate distances for these two cases
-  if (includeBehavior == "excludePixels" | returnDistances | closest) {
+  if (includeBehavior == "excludePixels" || returnDistances || closest) {
     if (equalRadii) {
       maxRad <- maxRadius[NROW(maxRadius)]
       minRad <- maxRadius[1]
@@ -653,17 +645,17 @@ cir <- function(landscape, coords, loci,
 
     # if distances are not required, then only need the inner circle and outer
     # circle distances. Don't waste resources on calculating all distances.
-    if (returnDistances | closest) {
+    if (returnDistances || closest) {
       matDT2 <- matDT
     } else {
       if (equalRadii) {
         # 0.71 is the sqrt of 1, so keep
         matDT2 <- matDT[matDT[, "rads"] >= (maxRad - 0.71) | matDT[, "rads"] <=
-                      (minRad + 0.71), , drop = FALSE]
+                          (minRad + 0.71), , drop = FALSE]
       } else {
         # 0.71 is the sqrt of 1, so keep
         matDT2 <- matDT[matDT[, "rads"] >= (matDT[, "maxRad"] - 0.71) | matDT[, "rads"] <=
-                      (matDT[, "minRad"] + 0.71), , drop = FALSE]
+                          (matDT[, "minRad"] + 0.71), , drop = FALSE]
       }
     } #  only pixels that are in inner or outer ring of pixels
 
@@ -685,17 +677,19 @@ cir <- function(landscape, coords, loci,
     d <- distanceFromEachPoint(b, a)
 
     if (closest) {
-      d <- d[order(d[, "rads"]), , drop = FALSE]
+      d <- d[order(d[, "rads"]), NULL, drop = FALSE]
       dups <- duplicated(d[, "to", drop = FALSE])
       d <- d[!dups, , drop = FALSE]
 
     }
 
-    if (includeBehavior == "excludePixels")
-      if (equalRadii)
-        d <- d[d[, "dists"] %<=% maxRad & d[, "dists"] %>=% minRad, , drop = FALSE]
-    else
-      d <- d[d[, "dists"] %<=% d[, "maxRad"] & d[, "dists"] %>=% d[, "minRad"], , drop = FALSE]
+    if (includeBehavior == "excludePixels") {
+      if (equalRadii) {
+        d <- d[d[, "dists"] %<=% maxRad & d[, "dists"] %>=% minRad, NULL, drop = FALSE]
+      } else {
+        d <- d[d[, "dists"] %<=% d[, "maxRad"] & d[, "dists"] %>=% d[, "minRad"], NULL, drop = FALSE]
+      }
+    }
 
     colnames(d)[which(colnames(d) == "to")] <- "indices"
     if (!returnDistances)

R/distanceFromEachPoint.R
@@ -281,7 +281,7 @@ distanceFromEachPoint <- function(from, to = NULL, landscape, angles = NA_real_,
 #'
 #' @inheritParams distanceFromEachPoint
 #'
-#' @param otherFromCols other colunms to use as 'from'
+#' @param otherFromCols other columns to use as 'from'
 #'
 #' @aliases pointDistance
 #' @export

R/initialize.R
@@ -606,9 +606,13 @@ long2UTM <- function(long) {
 #'       requireNamespace("raster", quietly = TRUE)) {
 #'     library(terra)
 #'     nx <- ny <- 100L
-#'     r <- rast(nrows = ny, ncols = nx, xmin = -nx/2, xmax = nx/2, ymin = -ny/2, ymax = ny/2)
+#'     r <- rast(nrows = ny, ncols = nx,
+#'               xmin = -nx/2, xmax = nx/2,
+#'               ymin = -ny/2, ymax = ny/2)
 #'     ## or with raster package:
-#'     # r <- raster::raster(nrows = ny, ncols = nx, xmn = -nx/2, xmx = nx/2, ymn = -ny/2, ymx = ny/2)
+#'     # r <- raster::raster(nrows = ny, ncols = nx,
+#'     #                     xmn = -nx/2, xmx = nx/2,
+#'     #                     ymn = -ny/2, ymx = ny/2)
 #'     map1 <- neutralLandscapeMap(r,
 #'                                 type = "nlm_mpd",
 #'                                 roughness = 0.65,

R/mapReduce.R
@@ -88,17 +88,8 @@ rasterizeReduced <- function(reduced, fullRaster, newRasterCols, mapcode = names
 
   colsToKeep <- c(mapcode, newRasterCols)
   BsumVec <- reduced[, ..colsToKeep][fullRasterVals] |> unique()
-
-  # This was removed by Eliot May 28, 2019 -- seems redundant -- if there are errors, this may be why
-  # if (length(newRasterCols) > 1) {
-  #   for (i in seq_along(newRasterCols)) {
-  #     BsumVec[is.na(get(newRasterCols[i])), c(newRasterCols[i]) := NA]
-  #   }
-  # } else {
-  #   browser()
-  #   BsumVec[is.na(get(newRasterCols)), c(newRasterCols) := NA]
-  # }
   setkeyv(BsumVec, "row_number")
+
   if (length(newRasterCols) > 1) {
     ras <- list()
     for (i in newRasterCols) {
@@ -118,7 +109,7 @@ rasterizeReduced <- function(reduced, fullRaster, newRasterCols, mapcode = names
     }
   } else {
     ras <- rasterFUN(fullRaster)
-    names(ras) <- names(rasterFUN(fullRaster))
+    names(ras) <- names(rasterFUN())
 
     if (is.factor(BsumVec[[newRasterCols]]) && is(ras, "SpatRaster")) {
       ras[] <- as.numeric(BsumVec[[newRasterCols]])

R/movement.R
@@ -124,7 +124,8 @@ crw <- function(agent, extent, stepLength, stddev, lonlat = FALSE, torus = FALSE
   xycolNames <- colnames(crds)
 
   if (!is.matrix(agent)) {
-    if (!any(vapply(c("SpatialPoints", "SpatVector"), inherits, x = agent, FUN.VALUE = logical(1)))) {
+    if (!any(vapply(c("SpatialPoints", "SpatVector"), inherits, x = agent,
+                    FUN.VALUE = logical(1)))) {
       if (is(agent, "SpatVector"))
         if (!identical("points", geomtype(agent)))
           stop("crs can only take SpatialPoints* or SpatVector points geometry")
@@ -173,10 +174,9 @@ crw <- function(agent, extent, stepLength, stddev, lonlat = FALSE, torus = FALSE
   if (inherits(agent, "SpatialPoints"))  {
     message("agent does not have columns named x1 and y1, which represent the 'previous' ",
             "locations. Assigning random values to those columns.")
-    # names(agent) <- x1y1colNames
     agent1 <- cbind(coords(agent), prevCoords)
     agent1 <- crw(agent1, extent = extent, stepLength = stepLength,
-                 stddev = stddev, lonlat = lonlat, torus = torus)
+                  stddev = stddev, lonlat = lonlat, torus = torus)
 
     if (returnMatrix %in% FALSE)
       if (!inherits(agent1, origClass)) {
@@ -232,4 +232,3 @@ crw <- function(agent, extent, stepLength, stddev, lonlat = FALSE, torus = FALSE
     return(agent)
   }
 }
-

R/probability.R
@@ -44,7 +44,7 @@ dwrpnorm2 <- function(theta, mu, rho, sd = 1, acc = 1e-05, tol = acc) {
   if (length(theta) != len)
     theta <- rep(theta, len)
 
-  if (rho < 0 | rho > 1)
+  if (rho < 0 || rho > 1)
     stop("rho must be between 0 and 1")
 
   var <- -2 * log(rho)

R/rings.R
@@ -39,9 +39,9 @@ rings <- function(landscape, loci = NA_real_, id = FALSE, minRadius = 2, maxRadi
                          circleMaxRadius = maxRadius, spreadProb = 1, id = TRUE,
                          returnDistances = TRUE, returnIndices = TRUE,
                          allowOverlap = allowOverlap, ...)
-  if (length(minRadius) > 1 | length(maxRadius) > 1) {
+  if (length(minRadius) > 1 || length(maxRadius) > 1) {
     len <- length(loci)
-    if (!(length(minRadius) == len | length(maxRadius) == len)) {
+    if (!(length(minRadius) == len || length(maxRadius) == len)) {
       warning("minRadius and maxRadius should be length 1 or same length as loci. ",
               "Recycling values which may not produce desired effects.")
     }

R/splitRaster.R
@@ -49,7 +49,7 @@
 #'
 splitRaster <- function(r, nx = 1, ny = 1, buffer = c(0, 0), path = NA, cl, rType = "FLT4S",
                         fExt = ".tif") {
-  if (!is.numeric(nx) | !is.numeric(ny) | !is.numeric(buffer)) {
+  if (!is.numeric(nx) || !is.numeric(ny) || !is.numeric(buffer)) {
     stop("nx, ny, and buffer must be numeric")
   }
   if (!is.integer(nx)) nx <- as.integer(nx)
@@ -133,8 +133,9 @@ splitRaster <- function(r, nx = 1, ny = 1, buffer = c(0, 0), path = NA, cl, rTyp
     if (is.character(tiles[[1]]))
       tiles <- lapply(tiles, terra::rast)
     if (grepl("INT", rType))
-      for (i in 1:length(tiles))
+      for (i in seq_along(tiles)) {
         tiles[[i]][] <- as.integer(tiles[[i]][])
+      }
     tiles
   }
 

commit 37c9ad36eb6442d2f7a7f52f672e0ce9e9683014

fix dt NULL in cir
problem introduced during cleanup in 7b53fc64d214b76c9d5eb2e0c85336df48d124b1

R/neighbourhood.R
@@ -677,17 +677,16 @@ cir <- function(landscape, coords, loci,
     d <- distanceFromEachPoint(b, a)
 
     if (closest) {
-      d <- d[order(d[, "rads"]), NULL, drop = FALSE]
+      d <- d[order(d[, "rads"]), , drop = FALSE]
       dups <- duplicated(d[, "to", drop = FALSE])
       d <- d[!dups, , drop = FALSE]
-
     }
 
     if (includeBehavior == "excludePixels") {
       if (equalRadii) {
-        d <- d[d[, "dists"] %<=% maxRad & d[, "dists"] %>=% minRad, NULL, drop = FALSE]
+        d <- d[d[, "dists"] %<=% maxRad & d[, "dists"] %>=% minRad, , drop = FALSE]
       } else {
-        d <- d[d[, "dists"] %<=% d[, "maxRad"] & d[, "dists"] %>=% d[, "minRad"], NULL, drop = FALSE]
+        d <- d[d[, "dists"] %<=% d[, "maxRad"] & d[, "dists"] %>=% d[, "minRad"], , drop = FALSE]
       }
     }
 